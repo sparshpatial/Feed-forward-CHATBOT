cadea4b54db50bbdbccb92424a7f8dbb12bde710
Sparsh Patial <patial.sparsh5@gmail.com>
2023-01-18 22:49:27 +0530

    nltk_-utils

---
+++ nltk_utils.py
@@ -0,0 +1,28 @@
+import nltk
+nltk.download('punkt')
+from nltk.stem.porter import PorterStemmer
+
+stemmer=PorterStemmer()
+
+def tokenize(sentence):
+    return nltk.word_tokenize(sentence)
+    
+    pass
+
+def stemming(word):
+    return stemmer.stem(word.lower())
+    pass
+
+def bag_of_words(tokenize_sentence,all_words):
+    pass
+
+#test for tokenising
+a="Is anyone there"
+print(a)
+a=tokenize(a)
+print(a)
+
+#test for the stemming
+words=["Organise","organises","organisation"]
+stemmed_words=[stemming(w)for w in words]
+print(stemmed_words)
