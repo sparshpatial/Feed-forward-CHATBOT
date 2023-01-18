62c5931df4a025fab6a41a7adb2eab60ba847a03
Sparsh Patial <patial.sparsh5@gmail.com>
2023-01-18 22:00:42 +0530

    nltk

---
+++ nltk.py
@@ -0,0 +1,12 @@
+import nltk
+nltk.download('punk')# pretrained tokeniser
+def tokenise(sentence):
+    
+    pass
+
+def stemming(word):
+    pass
+
+def bag_of_words(tokenise_sentence,all_words):
+    pass
+
